8877283fb7627c9d0fb87121505101702dbc22d1
Emeka Ozonze <mekaz.ozonze@gmail.com>
2024-03-23 02:13:17 +0000

    Initial Commit

---
+++ RentMovieMobile/RentMovieMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RentMovieMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RentMovieMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
